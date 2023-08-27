a5b0ef94c6ed166a099b1f607a05d3b055e0d736
Russell Purinton <russell.purinton@icloud.com>
2023-08-27 09:05:41 +0000

    bug_fix

--- src/ConfigLoader.php
+++ src/ConfigLoader.php
@@ -11,7 +11,7 @@ class ConfigLoader
 	{
 		exec("ls " . __DIR__ . "/conf.d/*.json", $configfiles);
 		foreach ($configfiles as $configfile) {
-			$section = substr($configfile, 0, strpos($configfile, ".conf"));
+			$section = substr($configfile, 0, strpos($configfile, ".json"));
 			$section = substr($section, strpos($section, "conf.d/") + 7);
 			$this->config[$section] = json_decode(file_get_contents($configfile), true);
 		}
