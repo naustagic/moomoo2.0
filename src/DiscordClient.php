60089b7a7e686e1d512a40e39ef40aac7a6e32be
Russell Purinton <russell.purinton@gmail.com>
2023-08-28 02:56:06 -0400

    Update DiscordClient.php

--- src/DiscordClient.php
+++ src/DiscordClient.php
@@ -74,12 +74,12 @@ class DiscordClient extends ConfigLoader
 		$interaction->respondWithMessage(MessageBuilder::new()->setContent("..."));
 	}
 
-	private function inbox($message, \Discord\Discord $discord)
+	private function inbox(\Discord\Parts\Channel\Message $message, \Discord\Discord $discord)
 	{
 		print_r($message);
-		$message = json_decode(json_encode($message), true);
-		$message["bot_id"] = $discord->id;
-		$message["guild_lang"] = $this->guild_langs[$message["guild_id"]];
+		$message_array = json_decode(json_encode($message), true);
+		$message_array["bot_id"] = $discord->id;
+		$message_array["guild_lang"] = $this->guild_langs[$message->guild_id];
 		$this->bunny->publish("moomoo_inbox", $message);
 	}
 
