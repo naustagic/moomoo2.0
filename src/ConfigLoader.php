80facb8a59ba200e83e033c339f5e7a95f71d88a
Russell Purinton <russell.purinton@gmail.com>
2023-08-28 04:32:29 -0400

    Update ConfigLoader.php

--- src/ConfigLoader.php
+++ src/ConfigLoader.php
@@ -6,24 +6,27 @@ require_once(__DIR__ . "/vendor/autoload.php");
 
 class ConfigLoader
 {
-	public $config = null;
-	public $lang = null;
-	public $commands = null;
+	public $config = [];
+	public $lang = [];
+	public $commands = [];
 
 	function __construct()
 	{
+		$configfiles = [];
 		exec("ls " . __DIR__ . "/conf.d/*.json", $configfiles);
 		foreach ($configfiles as $configfile) {
 			$section = substr($configfile, 0, strpos($configfile, ".json"));
 			$section = substr($section, strpos($section, "conf.d/") + 7);
 			$this->config[$section] = json_decode(file_get_contents($configfile), true);
 		}
+		$configfiles = [];
 		exec("ls " . __DIR__ . "/lang.d/*.json", $configfiles);
 		foreach ($configfiles as $configfile) {
 			$section = substr($configfile, 0, strpos($configfile, ".json"));
 			$section = substr($section, strpos($section, "lang.d/") + 7);
 			$this->lang[$section] = json_decode(file_get_contents($configfile), true);
 		}
+		$configfiles = [];
 		exec("ls " . __DIR__ . "/commands.d/*.json", $configfiles);
 		foreach ($configfiles as $configfile) {
 			$section = substr($configfile, 0, strpos($configfile, ".json"));
